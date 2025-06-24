3679662239410be15657db9242259196ad85139c
Josh French <parakoopa72@gmail.com>
2025-06-23 21:01:45 -0700

    initial commit

---
+++ Main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+// *Uncomment for Harmony*
+// using System.Reflection;
+// using HarmonyLib;
+
+namespace ExpandResourceReadout {
+
+    public class ExpandResourceReadoutSettings : ModSettings { }
+
+    public class ExpandResourceReadoutComponent : GameComponent {
+
+        private List<ThingCategoryDef> rootCategories;
+
+        public ExpandResourceReadoutComponent(Game g) : base() {
+            rootCategories = (from cat in DefDatabase<ThingCategoryDef>.AllDefs
+                                   where cat.resourceReadoutRoot
+                                   select cat).ToList<ThingCategoryDef>();
+        }
+
+        public override void LoadedGame() {
+            base.LoadedGame();
+            foreach(ThingCategoryDef thingCat in rootCategories) {
+                OpenRecursive(thingCat.treeNode, 32);
+            }
+        }
+
+        public override void StartedNewGame() {
+            base.StartedNewGame();
+            LoadedGame();
+        }
+
+        private void OpenRecursive(TreeNode_ThingCategory node, int mask) {
+            // not sure if needed, as all nodes should be openable
+            if(!node.Openable)
+                return;
+            node.SetOpen(mask, true);
+            foreach (TreeNode_ThingCategory child in node.ChildCategoryNodes) {
+                    OpenRecursive(child, mask);
+            }
+        }
+    }
+}
